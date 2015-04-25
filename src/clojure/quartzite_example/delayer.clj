acc5a4d0204b79abfdc142682e92d77af404a3cf
David Vilar <summun@gmail.com>
2015-04-25 11:59:01 +0100

    sending user data to the scheduled job

--- src/clojure/quartzite_example/delayer.clj
+++ src/clojure/quartzite_example/delayer.clj
@@ -1,27 +1,34 @@
 (ns quartzite-example.delayer
   (:require [clojurewerkz.quartzite.scheduler :as qs]
+            [clojurewerkz.quartzite.conversion :as qc]
             [clojurewerkz.quartzite.triggers :as t]
             [clojurewerkz.quartzite.jobs :as j]
             [clojurewerkz.quartzite.jobs :refer [defjob]]
-            [clojurewerkz.quartzite.schedule.simple :refer [schedule repeat-forever with-interval-in-seconds]]))
+            [clojurewerkz.quartzite.schedule.simple :as s]))
 
 (defjob SimpleJob [ctx]
-  (println "scheduled task triggered"))
+  (let [data (qc/from-job-data ctx)
+        message (data "message")]
+    (println message)))
 
 (defn- build-trigger []
   (t/build
     (t/with-identity (t/key "triggers.1"))
     (t/start-now)
-    (t/with-schedule (schedule (repeat-forever) (with-interval-in-seconds 2)))))
+    (t/with-schedule (s/schedule (s/repeat-forever) (s/with-interval-in-seconds 2)))))
 
-(defn- build-job []
+(defn- build-job [message]
   (j/build
     (j/of-type SimpleJob)
+    (j/using-job-data {"message" message})
     (j/with-identity (j/key "jobs.noop.1"))))
+
+(defn schedule [message]
+    (let [scheduler (-> (qs/initialize)qs/start)
+          trigger   (build-trigger)
+          job       (build-job message)]
+    (qs/schedule scheduler job trigger)))
 
 (defn init []
   (println "starting quartzite")
-  (let [scheduler (-> (-> (qs/initialize)qs/start))
-        trigger   (build-trigger)
-        job       (build-job)]
-    (qs/schedule scheduler job trigger)))
+  (schedule "every 2 seconds"))

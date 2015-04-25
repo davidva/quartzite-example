1c08c0b7f4a83f78c4b95e700b8f3e4b0033d82a
David Vilar <summun@gmail.com>
2015-04-25 13:40:54 +0100

    add form to schedule jobs

--- src/clojure/quartzite_example/handler.clj
+++ src/clojure/quartzite_example/handler.clj
@@ -2,11 +2,14 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
-            [ring.util.response :refer [resource-response content-type]]))
+            [ring.util.response :refer [resource-response content-type redirect]]))
 
 (defroutes app-routes
   (GET "/" [] (-> (resource-response "index.html" {:root "public"}) (content-type "text/html")))
+  (POST "/schedule" []
+    (println "scheduled")
+    (redirect "/"))
   (route/not-found "Not Found"))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (wrap-defaults app-routes (assoc-in site-defaults [:security :anti-forgery] false)))

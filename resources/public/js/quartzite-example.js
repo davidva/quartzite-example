e39356082f617429c623ad20f5f4d1e0a7a1022c
David Vilar <summun@gmail.com>
2015-04-26 12:12:16 +0100

    add job scheduled counter

--- resources/public/js/quartzite-example.js
+++ resources/public/js/quartzite-example.js
@@ -1,14 +1,32 @@
+var form = $("form")
+var jobsScheduledCounter = 0;
+
+var updateJobScheduledCounter = function() {
+  $(".jobs-scheduled-counter").text("" + jobsScheduledCounter);
+}
+
+var scheduleJob = function(message) {
+  $.ajax({
+    url: "/schedule",
+    method: "post",
+    data: "message=" + message
+  }).done(function(data) {
+    jobsScheduledCounter++;
+    updateJobScheduledCounter();
+
+    form.trigger("reset");
+  });
+}
+
 $(document).ready(function() {
-	var form = $("form")
+  updateJobScheduledCounter();
 
 	form.on("submit", function() {
-		$.ajax({
-			url: "/schedule",
-			method: "post",
-			data: form.serialize()
-		}).done(function(data) {
-			form.trigger("reset")
-		});
+    var message = $("input[name='message']").val();
+
+    if (message.length > 0) {
+      scheduleJob(message);
+    }
 		return false;
 	})
 })
